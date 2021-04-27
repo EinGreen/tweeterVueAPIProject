f14bf889a6287bb762539c4694e68a7a234a503e
Ein_Green <mbananola2002@gmail.com>
2021-04-27 17:25:05 -0600

    added a Page Header file just so I could commit something and say that I ACTUALLY remembered to push something to github. I feel like I am going to have a terrible time with this project

---
+++ src/components/PageHeaeder.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1>Welcome to Tweeter!</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "page-header",
+    }
+</script>
+
+<style scoped>
+
+</style>
